7f3a50b4227ac7a3d844ebd718b03fe1c52040d5
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-04-21 18:19:22 -0300

    Terminei projeto movimentos

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,34 @@
 let jogo = document.querySelector('#jogo')
 let personagem = document.querySelector('#personagem')
+let left_personagem = new String(window.getComputedStyle(personagem).getPropertyValue('left'))
+let valorLeft = Number(left_personagem.match(/\d/ig))
+let top_personagem = new String(window.getComputedStyle(personagem).getPropertyValue('top'))
+let valorTop = Number(top_personagem.match(/\d/ig))
 
 window.addEventListener('keydown',(event)=>{
     if (event.keyCode === 37){
         //seta esquerda
-        personagem.style.left = '40px'
+        if (valorLeft > 0){
+            valorLeft -= 40
+            personagem.style.left = valorLeft + 'px'
+        }
     } else if (event.keyCode === 38){
         //seta cima
-        personagem.style.top += 40+'px'
+        if (valorTop > 0){
+            valorTop -= 40
+            personagem.style.top = valorTop + 'px'
+        }
     } else if (event.keyCode === 39){
         //seta direita
-        personagem.style.left += 40+'px'
+        if (valorLeft < 360){
+            valorLeft += 40
+            personagem.style.left = valorLeft + 'px'
+        }
     } else if (event.keyCode === 40){
         //seta baixo
-        personagem.style.top += -40+'px'
+        if (valorTop < 360){
+            valorTop += 40
+            personagem.style.top = valorTop + 'px'
+        }
     }
 })
