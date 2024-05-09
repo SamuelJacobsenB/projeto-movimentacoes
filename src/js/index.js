1ec513d66c6032d9a82f6b500dbf4c94092b9cab
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-05-09 17:10:31 -0300

    Projeto terminado

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,9 @@ let valorLeft = Number(left_personagem.match(/\d/ig))
 let top_personagem = new String(window.getComputedStyle(personagem).getPropertyValue('top'))
 let valorTop = Number(top_personagem.match(/\d/ig))
 let pontuacao = document.querySelector('#pontuacao')
+let valorPont = 0
+
+//-------------------------------------------------
 
 window.addEventListener('keydown',(event)=>{
     if (event.code == 'ArrowLeft'){
@@ -32,31 +35,52 @@ window.addEventListener('keydown',(event)=>{
             personagem.style.top = valorTop + 'px'
         }
     }
+
+//--------------------------------
+
+    if(valorLeft == pessoa.posx && (valorTop-40) == pessoa.posy){
+        pessoa.remover()
+        pontuacao.innerHTML = 'Pontuação: ' + (valorPont+=1)
+        pessoa = new Npt()
+        pessoa.criar()
+        jogo.appendChild(quadrado)
+    }
 })
 
+//--------------------------------------------------
+
 class Npt {
+    tam = null
+    posx = null
+    posy = null
+    posicao = null
     constructor(){
         this.tam = 40
         this.posx = parseInt(Math.random()*19) * 40
         this.posy = parseInt(Math.random()*19) * 40
-        this.posicao = this.posx + this.posy
     }
     criar=()=>{
         let div = document.createElement('div')
         div.setAttribute('id','ponto')
-        div.setAttribute('style',`position: relative; height: ${this.tam}px; width: ${this.tam}px; left: ${this.posy}px; top: ${this.posx}px; background-color: blue;`)
-        jogo.appendChild(div)
+        div.setAttribute('style',`position: relative; height: ${this.tam}px; width: ${this.tam}px; left: ${this.posx}px; top: ${this.posy}px; background-color: blue;`)
+        quadrado = div
     }
     remover=()=>{
-        div.remove()
+        jogo.removeChild(quadrado)
+        quadrado = null
+        console.log(quadrado)
     }
 }
 
-let pessoa = new Npt()
-pessoa.criar()
+//---------------------------------------------------------
 
-let verificar = setInterval(()=>{    
-    if(valorLeft == pessoa.posy && valorTop == pessoa.posx){
-        pessoa.remover()
+var quadrado = null
+var pessoa = new Npt()
+
+let btn_iniciar = document.querySelector('#btn_iniciar')
+btn_iniciar.addEventListener('click',(evt)=>{
+    if(quadrado === null){
+        pessoa.criar()
+        jogo.appendChild(quadrado)
     }
-},20)
+})
